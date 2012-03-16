3d9f9d5dddbf93c880c0c27c2f5257336afd70f8
Viktor Oleksyn <bartezic@inbox.ru>
2012-03-16 08:48:24 +0200

    fixed admin form for ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ActiveRecord::Base
+  attr_accessible :kod, :ways_attributes
+  
   has_many :ways, :dependent => :destroy
   has_many :cities, :through => :ways
   accepts_nested_attributes_for :ways
