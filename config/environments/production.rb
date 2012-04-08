c10a469129d929bd477b4951a7114b65a14967de
Viktor Oleksyn <bartezic@inbox.ru>
2012-04-08 14:12:46 +0200

    fixed environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,7 @@ Bus::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = false
-  config.assets.debug = true
+  config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
@@ -16,7 +15,7 @@ Bus::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -32,7 +31,7 @@ Bus::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  # config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
